b70005d16befab5ae00b0666499ec0e1516e58fa
drbarq <J.Tustin@gmail.com>
2019-09-05 14:35:02 -0600

    adding in search

--- food/src/components/SearchBar.js
+++ food/src/components/SearchBar.js
@@ -1,21 +1,43 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, TextInput, StyleSheet } from 'react-native'
 import { Feather } from '@expo/vector-icons'
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
     return (
-        <View style={styles.background}>
-            <Feather name="search" size={30}></Feather>
-            <Text>Search Bar</Text>
+        <View style={styles.backgroundStyle}>
+            <Feather 
+                name="search" 
+                style={styles.iconStyle}
+            />
+            <TextInput 
+                placeholder="Search"
+                style={styles.inputStyle}
+                value={term}
+                autoCapitalize="none"
+                autoCorrect={false}
+                onEndEditing={onTermSubmit}
+                onChangeText={onTermChange}
+            />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    background: {
+    backgroundStyle: {
         backgroundColor: '#F0EEEE',
         height: 50,
         borderRadius: 5,
+        marginHorizontal: 15,
+        flexDirection: 'row',
+        marginTop: 10
+    },
+    inputStyle: {
+        flex: 1,
+        fontSize: 18
+    },
+    iconStyle: {
+        fontSize: 35,
+        alignSelf: 'center',
         marginHorizontal: 15
     }
 })
